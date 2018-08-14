76b3b29fba27b716f7c0e075ee60b63dd60e1da5
Ben Miller <bmiller@rackspace.com>
2018-08-14 03:25:49 -0400

    add xref to explain kernel methods

--- lib/better_ipaddr/kernel_method.rb
+++ lib/better_ipaddr/kernel_method.rb
@@ -1,12 +1,14 @@
 require "better_ipaddr/classes"
 
 module Kernel
+  # @see IPAddr::Base.from
   def IPAddr(object, exception: false, classful: false)
     IPAddr::Base.from(object, exception: exception, classful: classful)
   end
 end
 
 class IPAddr
+  # @see IPAddr::Base.host_from
   def self.Host(object, exception: false)
     Base.host_from(object, exception: exception)
   end
